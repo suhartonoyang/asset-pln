bb8ef8f3e44d89d7b30678794b42f26f0cbfc758
51974 <suhartono@fifgroup.astra.co.id>
2022-05-23 09:57:31 +0700

    initiate project

---
+++ src/main/java/com/project/assetpln/AssetPlnApplication.java
@@ -0,0 +1,13 @@
+package com.project.assetpln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetPlnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetPlnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/assetpln/AssetPlnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.assetpln;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetPlnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
